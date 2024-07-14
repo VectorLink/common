97aa7db33a743218ada16d96587f47217af0187a
李军 <1107746668@qq.com>
2024-07-14 12:28:31 +0800

    新增微服务名

---
+++ src/main/java/com/vectorlink/common/ServiceNameConstants.java
@@ -0,0 +1,10 @@
+package com.vectorlink.common;
+
+/**
+ * @author lijun
+ */
+public class ServiceNameConstants {
+    public static final String BASE_COMPANY="company";
+    public static final String BASE_FINANCE="finance";
+    public static final String BASE_ORDER="order";
+}
